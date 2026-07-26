0350c9e8f14c1c885f60b0b5b8416463f636b294
techdining01 <amooade12@gmail.com>
2026-07-26 08:31:14 +0100

    custom command: ensure_superuser

--- build.sh
+++ build.sh
@@ -3,10 +3,10 @@
 set -o errexit
 
 # Install dependencies
-pip install -r requirements.txt
+pip install -r requirements.txt && python manage.py collectstatic --noinput
 
-# Run migrations
-python manage.py migrate
+# Run migrations, seed all data, and run server
+python manage.py migrate --noinput && python manage.py seed_all && daphne -b 0.0.0.0 -p $PORT school_sms.asgi:application
 
-# If using a custom command:
-python manage.py seed_all
+
+
--- exams/management/commands/seed_all.py
+++ exams/management/commands/seed_all.py
@@ -5,17 +5,17 @@ class Command(BaseCommand):
     help = "Seed entire system (accounts, exams, payments, pickups, logs)"
 
     def handle(self, *args, **kwargs):
+        self.stdout.write(self.style.WARNING("Seeding Superuser..."))
+        call_command("ensure_superuser")
+
         self.stdout.write(self.style.WARNING("Seeding Accounts..."))
         call_command("seed_accounts")
 
-        self.stdout.write(self.style.WARNING("Seeding BrillsPay..."))
+        self.stdout.write(self.style.WARNING("Seeding MPay..."))
         call_command("seed_brillspay")
 
         self.stdout.write(self.style.WARNING("Seeding Pickups..."))
         call_command("seed_pickups")
-
-        self.stdout.write(self.style.WARNING("Seeding System Logs..."))
-        call_command("seed_logs")
 
         self.stdout.write(self.style.WARNING("Seeding Exams..."))
         call_command("seed_exams")
