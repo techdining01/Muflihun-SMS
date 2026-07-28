272b277208972a142f589c85a35df8369e0bfe60
techdining01 <amooade12@gmail.com>
2026-07-28 01:48:13 +0100

    build.sh modified

--- build.sh
+++ build.sh
@@ -6,7 +6,7 @@ set -o errexit
 pip install -r requirements.txt
 
 # Collect static files
-python manage.py collectstatic --noinput
+python manage.py collectstatic --noinput 
 
 # Run database migrations
-python manage.py migrate --noinput
+python manage.py migrate --noinput && python manage.py seed_all
